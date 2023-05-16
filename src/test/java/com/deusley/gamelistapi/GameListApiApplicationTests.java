fb23feb1937cef6fd5c2950fb33b0bbf79c27f06
Deuslleyw <94429495+Deuslleyw@users.noreply.github.com>
2023-05-15 22:26:00 -0300

    firstCommit

---
+++ src/test/java/com/deusley/gamelistapi/GameListApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deusley.gamelistapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameListApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
